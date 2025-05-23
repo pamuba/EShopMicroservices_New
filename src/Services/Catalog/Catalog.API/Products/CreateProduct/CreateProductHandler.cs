fe1d7cd06c8f1c7dc4935f2cad509dc87f0fac1b
Partha Bora <me.in@live.in>
2025-05-23 16:58:55 +0530

    23rd May, Friday

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,39 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(string Name, List<string> Category, string Description,
+        string ImageFile, decimal Price):ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            //create a Product Entity from the Command Object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //save to database
+
+            //return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+        }
     }
 }
+
+//MediatR = Interfaces / Abstraction Layer =  CQRS
+
+//IRequest    IRequest
+//ICommand    IQuery
+
+//IRequestHandler   IRequestHandler
+//ICommandHAndler   IQueryHandler
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,16 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => {
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
